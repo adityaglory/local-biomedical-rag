f7db5809a5b1419ed982ca46e8f9dcb663e4fc92
Glory Aditya Jauzaulbahi <72372538+adityaglory@users.noreply.github.com>
2025-12-17 13:33:53 +0700

    Refactor LocalRAG retriever settings for efficiency
    Updated the retriever settings to increase the number of text segments processed and modified the search parameters for improved context variation.

--- rag_engine.py
+++ rag_engine.py
@@ -2,7 +2,6 @@
 import os
 import warnings
 
-# Sesuaikan import ini sama workaround lo tadi ya!
 from langchain_classic.chains import RetrievalQA 
 from langchain_core.prompts import PromptTemplate
 from langchain_community.vectorstores import Chroma
@@ -13,19 +12,13 @@ warnings.filterwarnings("ignore")
 
 class LocalRAG:
     def __init__(self, db_path="./chroma_db", llm_model="llama3.2"):
-        # Kita load database & model SEKALI aja pas server nyala.
-        # Ini jauh lebih efisien daripada load tiap kali ada pertanyaan.
         print("⚙️  Loading RAG Engine...")
         
         self.embedding_function = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
         self.db = Chroma(persist_directory=db_path, embedding_function=self.embedding_function)
-        # self.retriever = self.db.as_retriever(search_kwargs={"k": 3})
-        # TUNING PART 🔧
-        # Kita ganti jadi MMR biar variasi konteksnya lebih luas
-        # k=6: Kita kasih makan AI 6 potongan teks (sebelumnya cuma 3)
-        # fetch_k=20: Cari 20 kandidat dulu, baru filter jadi 6 yang paling unik
+        
         self.retriever = self.db.as_retriever(
-            search_type="similarity", # Balik ke similarity biasa tapi jumlah banyak
+            search_type="similarity",
             search_kwargs={"k": 12} 
         )
         
@@ -36,7 +29,7 @@ class LocalRAG:
         self.llm = ChatOllama(
             model=llm_model, 
             temperature=0.3,
-            base_url=ollama_base_url # <--- Tambahin parameter ini
+            base_url=ollama_base_url
         )
         
         template = """
